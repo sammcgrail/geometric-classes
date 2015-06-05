651a29353feddf40e6222a7c3dcc88dd32123bd4
sammcgrail <sammcgrail@gmail.com>
2015-06-05 09:59:24 -0400

    4/5 for circle test

--- lib/circle.rb
+++ lib/circle.rb
@@ -5,11 +5,40 @@ class Circle
     @radius = radius
     @x = x
     @y = y
+    # @diameter = diameter
+    # @area = area
+    # @perimeter = perimeter
   end
+
+  # Alternate method from attr_reader
+  # def radius
+  #   @radius
+  # end
+  #
+  # def x
+  #   @x
+  # end
+  #
+  # def y
+  #   @y
+  # end
+
+  # def diameter
+  #   @diameter = @radius * 2
+  # end
+
+  # def area
+  #   #pseudo
+  # end
+  #
+  # def perimeter
+  #   #pseudo
+  # end
+
 end
 
-
-
+#
+# Circle.
 
 
 pi = Math::PI
